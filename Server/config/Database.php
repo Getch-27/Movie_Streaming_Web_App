4c1ee2e1bb03843221436215e7d6fac7e9b042d1
getch <getahunlast@gmail.com>
2023-12-15 07:19:28 -0800

    able to connect to database

---
+++ Server/config/Database.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database {
+    //variable name for database
+    private $host = 'localhost';
+    private $db_name = 'movie_streaming_db';
+    private $username = 'getch1';
+    private $password = '123';
+    public $conn;
+    
+    // function to establish connection
+    public function connect() {
+        $this->conn = null;
+
+        try {
+            $this->conn = new PDO(
+                'mysql:host=' . $this->host . ';dbname=' . $this->db_name,
+                $this->username,
+                $this->password
+            );
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "connection successfully established";
+        } catch (PDOException $e) {
+            echo 'Connection Error: ' . $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
---
+++ Server/index.php
@@ -0,0 +1,5 @@
+<?php
+ include_once ("config/Database.php");
+  $databse =new Database;
+  $db = $databse->connect();
+?>
