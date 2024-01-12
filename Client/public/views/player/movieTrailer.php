44b63b5828251392088bdfad2c2f734850872028
Getch-27 <getahunlast@gmail.com>
2024-01-12 01:12:02 -0800

    style change on trailer page

--- Client/public/views/player/movieTrailer.php
+++ Client/public/views/player/movieTrailer.php
@@ -6,18 +6,23 @@ $triler_link =  urldecode($_GET['trailer']);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>triler</title>
 </head>
+
 <body>
     <?php include_once("../../components/header.php"); ?>
     <!-- Your PHP code or HTML content here -->
-
-    <div style="max-width: 800px; margin: 0 auto;" class=" pt-24 rounded-md">
-    <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $triler_link ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    <div class=" flex items-center justify-center py-32">
+        <div style="max-width: 560px;" class=" mx-auto  bg-black rounded-md shadow-md overflow-hidden">
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/<?php echo $triler_link ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
     </div>
+
     <?php include_once("../../components/footer.php"); ?>
 </body>
+
 </html>
