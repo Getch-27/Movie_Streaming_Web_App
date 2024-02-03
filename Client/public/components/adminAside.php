96a322f4254a12a3447fde44524aff4a7037716b
Getch-27 <getahunlast@gmail.com>
2024-02-03 13:49:22 -0800

    miner change

--- Client/public/components/adminAside.php
+++ Client/public/components/adminAside.php
@@ -35,13 +35,15 @@ $totalCreators = count($allCreators);
 
 <head>
     <meta charset="UTF-8">
+    <base href="http://localhost/Movie_Streaming_Web_App/client/public/components/adminAside2.php" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../styles.css">
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
     <title>Document</title>
 </head>
 
 <body>
-    <div class="flex w-full">
+    <div class="grid grid-cols-6">
         <aside class=" bg-gray-900 w-64 h-screen sticky top-0 overflow-hidden">
             <div class=" w-full flex items-center p-5 justify-evenly">
                 <img src="../../public/images/tune.png" class=" w-10" srcset="">
@@ -51,43 +53,19 @@ $totalCreators = count($allCreators);
             <div class="  h-full pt-8">
                 <nav class="  p-8">
                     <ul class="">
-                        <li class="p-4 flex items-center justify-evenly hover:bg-gray-600 text-lg text-white shadow-sm rounded-lg">
+                        <li class=" border-blue-700 p-4 flex items-center justify-evenly hover:bg-gray-600 text-lg text-white shadow-sm rounded-lg">
                             <img src="../../public/images/dashboard.png" class=" w-6" alt="">
-                            <a href="">Dashboard</a>
+                            <a href="../views/admin/Dashboard.php">Dashboard</a>
                         </li>
                         <li class=" p-4 flex items-center justify-evenly hover:bg-gray-600 text-lg text-white shadow-sm rounded-lg">
                             <img src="../../public/images/add-user.png" class=" w-6" alt="">
-                            <a href="">Add Creator</a>
+                            <a href="../views/admin/addCreator.php">Add Creator</a>
                         </li>
                         <li class=" p-4 flex items-center justify-evenly hover:bg-gray-600 text-lg text-white shadow-sm rounded-lg">
                             <img src="../../public/images/block-user.png" class=" w-6" alt="">
-                            <a href="">Delete Creator</a>
+                            <a href="../views/admin/deleteCreator.php">Delete Creator</a>
                         </li>
                     </ul>
                 </nav>
             </div>
         </aside>
-
-        <!-- Dashboard -->
-        <div class="grid grid-cols-3 bg-gray-700 gap-4 align-middle">
-            <div class=" w-64 h-40 bg-gray-200 rounded-md shadow-sm p-4">
-                <img src="../../public/images/multiple-users-silhouette.png" class=" w-16" alt="">
-                <h1 class=" text-3xl font-medium"><?php echo $totalUsers ?></h1>
-                <h6>Total Users</h6>
-            </div>
-            <div class=" w-64 h-40 bg-gray-200 rounded-md shadow-sm p-4">
-                <img src="../../public/images/content-creator.png" class=" w-16" alt="">
-                <h1 class=" text-3xl font-medium"><?php echo $totalCreators ?></h1>
-                <h6>Total content Creators</h6>
-            </div>
-            <div class=" w-64 h-40 bg-gray-200 rounded-md shadow-sm p-4">
-                <img src="../../public/images/film-slate.png" class=" w-16" alt="">
-                <h1 class=" text-3xl font-medium"><?php echo $allMovies ?></h1>
-                <h6>Total uploaded movies</h6>
-            </div>
-        </div>
-    </div>
-
-</body>
-
-</html>
