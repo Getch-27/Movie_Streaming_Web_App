686ce6ba61a61c786ea2b017b86da9e7493347d8
Getch-27 <getahunlast@gmail.com>
2024-02-04 15:16:18 -0800

    un usable code removed

--- Server/controllers/adminControler.php
+++ Server/controllers/adminControler.php
@@ -2,7 +2,7 @@
 include_once("../../models/Admin.php");
 class adminController extends Admin
 {
-    private $creator;
+    
 
     //pass username and password to creator model
     public function adminLoginCont($data)
